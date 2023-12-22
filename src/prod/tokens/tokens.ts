65dc495ec07d65bbd1469cbbcf2d3c204b16f83b
McGrady00H <281130306@qq.com>
2023-12-22 15:49:26 +0800

    update

--- src/prod/tokens/tokens.ts
+++ src/prod/tokens/tokens.ts
@@ -13,6 +13,66 @@ const baseURL = 'https://bitswap-finance.oss-ap-southeast-1.aliyuncs.com/tokens/
 
 export const tokens: TokenInfo[] = [
     {
+        name: "DAII Token",
+        symbol: TokenSymbol.DAII,
+        icon: baseURL + 'daii.svg',
+        chains: [ChainId.EthereumMainnet],
+        contracts: {
+            [ChainId.EthereumMainnet]: {
+                address: '0x1981E32C2154936741aB6541a737b87C68F13cE1',
+                decimal: 18,
+            },
+        },
+    },
+    {
+        name: "Bounce Token",
+        symbol: TokenSymbol.AUCTION,
+        icon: baseURL + 'auction.png',
+        chains: [ChainId.EthereumMainnet],
+        contracts: {
+            [ChainId.EthereumMainnet]: {
+                address: '0xA9B1Eb5908CfC3cdf91F9B8B3a74108598009096',
+                decimal: 18,
+            },
+        },
+    },
+    {
+        name: "BitStable Finance Token",
+        symbol: TokenSymbol.BSSB,
+        icon: baseURL + 'bssb.png',
+        chains: [ChainId.EthereumMainnet],
+        contracts: {
+            [ChainId.EthereumMainnet]: {
+                address: '0xda31D0d1Bc934fC34F7189E38A413ca0A5e8b44F',
+                decimal: 18,
+            },
+        },
+    },
+    {
+        name: "Multibit Token",
+        symbol: TokenSymbol.MUBI,
+        icon: baseURL + 'mubi.png',
+        chains: [ChainId.EthereumMainnet],
+        contracts: {
+            [ChainId.EthereumMainnet]: {
+                address: '0x38e382F74dfb84608F3C1F10187f6bEf5951DE93',
+                decimal: 18,
+            },
+        },
+    },
+    {
+        name: "ORDI Multibit Bridge Token",
+        symbol: TokenSymbol.ORDI,
+        icon: baseURL + 'ordi.png',
+        chains: [ChainId.EthereumMainnet],
+        contracts: {
+            [ChainId.EthereumMainnet]: {
+                address: '0xe2aE1a99bBd2eFab0a5C38f2146B7aCE61aBC5cE',
+                decimal: 18,
+            },
+        },
+    },
+    {
         name: 'USD Coin',
         symbol: TokenSymbol.USDC,
         icon: baseURL + 'usdc.svg',
@@ -72,66 +132,6 @@ export const tokens: TokenInfo[] = [
         contracts: {
             [ChainId.EthereumMainnet]: {
                 address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "Bounce Token",
-        symbol: TokenSymbol.AUCTION,
-        icon: baseURL + 'auction.png',
-        chains: [ChainId.EthereumMainnet],
-        contracts: {
-            [ChainId.EthereumMainnet]: {
-                address: '0xA9B1Eb5908CfC3cdf91F9B8B3a74108598009096',
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "BitStable Finance Token",
-        symbol: TokenSymbol.BSSB,
-        icon: baseURL + 'bssb.png',
-        chains: [ChainId.EthereumMainnet],
-        contracts: {
-            [ChainId.EthereumMainnet]: {
-                address: '0xda31D0d1Bc934fC34F7189E38A413ca0A5e8b44F',
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "Multibit Token",
-        symbol: TokenSymbol.MUBI,
-        icon: baseURL + 'mubi.png',
-        chains: [ChainId.EthereumMainnet],
-        contracts: {
-            [ChainId.EthereumMainnet]: {
-                address: '0x38e382F74dfb84608F3C1F10187f6bEf5951DE93',
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "DAII Token",
-        symbol: TokenSymbol.DAII,
-        icon: baseURL + 'daii.svg',
-        chains: [ChainId.EthereumMainnet],
-        contracts: {
-            [ChainId.EthereumMainnet]: {
-                address: '0x1981E32C2154936741aB6541a737b87C68F13cE1',
-                decimal: 18,
-            },
-        },
-    },
-    {
-        name: "ORDI Multibit Bridge Token",
-        symbol: TokenSymbol.ORDI,
-        icon: baseURL + 'ordi.png',
-        chains: [ChainId.EthereumMainnet],
-        contracts: {
-            [ChainId.EthereumMainnet]: {
-                address: '0xe2aE1a99bBd2eFab0a5C38f2146B7aCE61aBC5cE',
                 decimal: 18,
             },
         },
